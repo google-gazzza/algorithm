0c9ef8d5deec3f18812600a777b47363ba9691e3
seokhyun <hsh2438@naver.com>
2019-10-22 00:06:47 +0900

    leetcode

--- 182_Duplicate_Emails/dongho.sql
+++
@@ -1,8 +0,0 @@
---https://leetcode.com/problems/duplicate-emails/
---Runtime: 680 ms, faster than 92.15% of MS SQL Server online submissions for Duplicate Emails.
---Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Duplicate Emails.
-
-SELECT Email
-  FROM Person
- GROUP BY Email
-HAVING COUNT(Email) > 1
