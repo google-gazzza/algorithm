1cc27deb63c1c96da423d34da525eb1e0cd1ab57
netty1010 <56634703+netty1010@users.noreply.github.com>
2019-11-01 23:40:34 +0900

    0을 False로 변경

--- leetcode/easy/204_count_primes/dosun.py
+++ leetcode/easy/204_count_primes/dosun.py
@@ -29,7 +29,7 @@ import math
 
 class Solution:
     def countPrimes(self, n: int) -> int:
-        if n < 3: return 0
+        if n < 3: return False
         primes = [False, False] + [True] * (n - 2)
         for i in range(2, int(math.sqrt(n)) + 1):
             if primes[i]:
