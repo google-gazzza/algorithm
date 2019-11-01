e5089b456392a3d786d38f86f7501d63b0956ed8
netty1010 <56634703+netty1010@users.noreply.github.com>
2019-11-01 23:36:36 +0900

    slice 사용하여 속도 개선

--- leetcode/easy/204_count_primes/dosun.py
+++ leetcode/easy/204_count_primes/dosun.py
@@ -3,7 +3,6 @@
 Runtime: 428 ms, faster than 71.10% of Python3 online submissions for Count Primes.
 Memory Usage: 25.4 MB, less than 79.31% of Python3 online submissions for Count Primes.
 """
-
 import math 
 
 class Solution:
@@ -17,3 +16,23 @@ class Solution:
                     primes[j] = False
 
         return sum(primes)
+
+
+"""https://leetcode.com/problems/count-primes/
+
+More fast
+
+Runtime: 152 ms, faster than 96.02% of Python3 online submissions for Count Primes.
+Memory Usage: 44.9 MB, less than 27.59% of Python3 online submissions for Count Primes.
+"""
+import math 
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 3: return 0
+        primes = [False, False] + [True] * (n - 2)
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if primes[i]:
+                primes[i*i:n:i] = [False] * len(range(i*i, n, i))  
+                
+        return primes.count(True)
