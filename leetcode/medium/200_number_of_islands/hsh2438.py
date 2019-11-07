8e78f2c54ab20413a57c8a80071f4eaf7b270d86
seokhyun <hsh2438@naver.com>
2019-11-07 23:00:28 +0900

    https://leetcode.com/problems/number-of-islands

--- leetcode/medium/200_number_of_islands/hsh2438.py
+++ leetcode/medium/200_number_of_islands/hsh2438.py
@@ -1,22 +1,16 @@
 """
 https://leetcode.com/problems/number-of-islands/
-Runtime: 328 ms, faster than 5.19% of Python3 online submissions for Number of Islands.
-Memory Usage: 31.5 MB, less than 5.13% of Python3 online submissions for Number of Islands.
+Runtime: 140 ms, faster than 95.64% of Python3 online submissions for Number of Islands.
+Memory Usage: 14 MB, less than 45.30% of Python3 online submissions for Number of Islands.
 """
-
-
-import numpy as np
 
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        grid = np.array(grid)
-        grid = grid.astype('int')
-
         def recursive(i, j):
             if i < 0 or j < 0 or i == len(grid) or j == len(grid[i]):
                 return 0
-            if grid[i][j] == 1:
+            if grid[i][j] == "1":
                 grid[i][j] = 0
                 recursive(i - 1, j)
                 recursive(i + 1, j)
@@ -28,7 +22,7 @@ class Solution:
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
-                if grid[i][j] == 1:
+                if grid[i][j] == "1":
                     count += 1
                     recursive(i, j)
 
