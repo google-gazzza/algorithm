406a769496570d4971271ac47ee5c202bd3d5763
seokhyun <hsh2438@naver.com>
2019-12-31 17:43:59 +0900

    Update hsh2438.py

--- leetcode/easy/350_intersection_of_two_arrays_II/hsh2438.py
+++ leetcode/easy/350_intersection_of_two_arrays_II/hsh2438.py
@@ -1,6 +1,6 @@
 """
 https://leetcode.com/problems/intersection-of-two-arrays-ii/
-Runtime: 44 ms, faster than 90.37% of Python3 online submissions for Intersection of Two Arrays II.
+Runtime: 40 ms, faster than 96.99% of Python3 online submissions for Intersection of Two Arrays II.
 Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Intersection of Two Arrays II.
 """
 
@@ -10,12 +10,6 @@ from collections import Counter
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        counter = Counter(nums1) & Counter(nums2)
-        result = []
-        for key in counter.keys():
-            for _ in range(counter[key]):
-                result.append(key)
-        
-        return result
+        return (Counter(nums1) & Counter(nums2)).elements()
         
         
