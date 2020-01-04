c0cf142118ba58529ebe355458b93dd22e5cd6f5
codename6027 <solver6027@gmail.com>
2020-01-04 17:04:48 +0900

    refactor(coding-challange/codewars/8kyu/is-n-divisible-by-x-and-y.py)

--- codewars/8kyu/is-n-divisible-by-x-and-y/dduck-is-n-divisible-by-x-and-y.py
+++ codewars/8kyu/is-n-divisible-by-x-and-y/dduck-is-n-divisible-by-x-and-y.py
@@ -2,7 +2,7 @@
 
 is_divisible = lambda n, x, y: (n % x == 0 and n % y == 0)
 
-print(is_divisible(3, 3, 4), False)
-print(is_divisible(12, 3, 4), True)
-print(is_divisible(8, 3, 4), False)
-print(is_divisible(48, 3, 4), True)
+assert (is_divisible(3, 3, 4) == False)
+assert (is_divisible(12, 3, 4) == True)
+assert (is_divisible(8, 3, 4) == False)
+assert (is_divisible(48, 3, 4) == True)
