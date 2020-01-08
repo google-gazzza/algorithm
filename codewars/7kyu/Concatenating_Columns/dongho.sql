c53b30af9b118782ba32136c87002699fe989337
donghokim-DBA <dongho527@naver.com>
2020-01-08 21:43:39 +0900

    Concatenating Columns

---
+++ codewars/7kyu/Concatenating_Columns/dongho.sql
@@ -0,0 +1,15 @@
+--https://www.codewars.com/kata/59440034e94fae05b2000073/train/sql
+--PostgreSQL 9.6
+
+-- mysql
+SELECT prefix || ' ' || first || ' ' || last || ' ' || suffix AS title
+  FROM names
+
+-- sql server(1)
+SELECT prefix + ' ' + first + ' ' + last + ' ' + suffix AS title
+  FROM names
+
+-- sql server(2)
+SELECT prefix + space(1) + first + space(1) + last + space(1) + suffix AS title
+  FROM names
+
