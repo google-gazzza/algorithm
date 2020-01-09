192d462c728e47e70e96a262244d84eac2144c3f
jmp7786 <jmp7786@gmail.com>
2020-01-09 13:42:59 +0900

    [Easy] 111. Minimum Depth Of Binary Tree

---
+++ leetcode/easy/111_minimum_depth_of_binary_tree/gaebari.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Minimum Depth of Binary Tree.
+// Memory Usage: 39.4 MB, less than 95.31% of Java online submissions for Minimum Depth of Binary Tree.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return ( (left == 0 || right == 0) ? left + right : Math.min(left,right) ) + 1;
+    }
+}
