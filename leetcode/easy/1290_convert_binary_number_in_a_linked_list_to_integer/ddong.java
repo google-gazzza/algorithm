eaa8a7868e1c773cff86e3cabef2f8caa8eaeed5
gloryjewel <sgloryjewel@gmail.com>
2020-01-11 23:16:29 +0900

    change comment

--- leetcode/easy/1290_convert_binary_number_in_a_linked_list_to_integer/ddong.java
+++ leetcode/easy/1290_convert_binary_number_in_a_linked_list_to_integer/ddong.java
@@ -1,9 +1,9 @@
-// convert-binary-number-in-a-linked-list-to-integer
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
 // Runtime: 0 ms, faster than 100.00% of Java online submissions for Convert Binary Number in a Linked List to Integer.
 // Memory Usage: 34.2 MB, less than 100.00% of Java online submissions for Convert Binary Number in a Linked List to Integer.
 /**
- * Definition for singly-linked list. public class ListNode { 
- * int val; ListNodeext; ListNode(int x) { val = x; } }
+ * Definition for singly-linked list. public class ListNode { int val;
+ * ListNodeext; ListNode(int x) { val = x; } }
  */
 class Solution {
     public int getDecimalValue(ListNode head) {
