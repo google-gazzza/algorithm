09d468dde30e5b020e70e5a94c55437f0ae5795b
jmp7786 <jmp7786@gmail.com>
2020-01-11 19:22:00 +0900

    [Easy] 104. Minimum Depth Of Binary Tree

---
+++ leetcode/easy/104_maximum_depth_of_binary_tree/gaebari.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return ( (left == 0 || right == 0) ? left + right : Math.min(left,right) ) + 1;
+    }
+}
