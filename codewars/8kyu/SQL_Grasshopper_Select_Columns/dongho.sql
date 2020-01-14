616d5b05c6b3d9c879539fdda5283d3c9bd090d7
DONGHO KIM <56637539+DONGHO-KIM-DBA@users.noreply.github.com>
2020-01-14 22:39:33 +0900

    주석 수정

--- codewars/8kyu/SQL_Grasshopper_Select_Columns/dongho.sql
+++ codewars/8kyu/SQL_Grasshopper_Select_Columns/dongho.sql
@@ -1,7 +1,7 @@
 --https://www.codewars.com/kata/582365c18917435ab3000020/train/sql
 --Time: 1517ms Passed: 6 Failed: 0
 
--- 1. custard¸¦ Á¦¿ÜÇÑ ¸ðµçÄÃ·³ Ãâ·Â
+-- 1. custardë¥¼ ì œì™¸í•œ ëª¨ë“ ì»¬ëŸ¼ ì¶œë ¥
 --PostgreSQL 9.6
 SELECT custid, custname, custstate
   FROM customers
