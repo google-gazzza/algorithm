be09cc9cc1c8e4703977d963f28f055288286276
이다영 <leedayoung111@gmail.com>
2020-01-16 13:37:41 +0900

    문연님에게 혼나서 정렬 맞춤

--- leetcode/easy/561_array_partition_i/leedy1.java
+++ leetcode/easy/561_array_partition_i/leedy1.java
@@ -5,10 +5,10 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
         int result = 0;
-		Arrays.sort(nums);
-		for(int i=0; i<nums.length-1; i+=2) {
-			result+= Math.min(nums[i], nums[i+1]);
-		}
-		return result;
+	Arrays.sort(nums);
+	for(int i=0; i<nums.length-1; i+=2) {
+		result+= Math.min(nums[i], nums[i+1]);
+	}
+	return result;
     }
 }
