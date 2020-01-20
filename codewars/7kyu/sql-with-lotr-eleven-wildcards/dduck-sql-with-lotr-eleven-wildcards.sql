83795b8cef964df94d7b6ec2a619b35015e10a64
codename6027 <solver6027@gmail.com>
2020-01-21 02:50:14 +0900

    refactor(codewars/7kyu/sql-with-lotr-eleven-wildcards.sql)

--- codewars/7kyu/sql-with-lotr-eleven-wildcards/dduck-sql-with-lotr-eleven-wildcards.sql
+++ codewars/7kyu/sql-with-lotr-eleven-wildcards/dduck-sql-with-lotr-eleven-wildcards.sql
@@ -1,5 +1,6 @@
 --https://www.codewars.com/kata/5ad90fb688a0b74111000055/train/sql
 
-SELECT concat_ws(' ', initcap(firstname), initcap(lastname)) AS shortlist
+SELECT initcap(firstname || ' ' || lastname) AS shortlist
 FROM Elves
-WHERE firstname LIKE '%tegil%' OR lastname LIKE '%astar%'
+WHERE firstname LIKE '%tegil%'
+   OR lastname LIKE '%astar%'
