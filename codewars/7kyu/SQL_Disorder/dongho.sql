3ce25c91b70dd372611d25cc37368365374ec19f
donghokim-DBA <dongho527@naver.com>
2020-01-27 19:19:49 +0900

    SQL_Disorder

---
+++ codewars/7kyu/SQL_Disorder/dongho.sql
@@ -0,0 +1,8 @@
+--https://www.codewars.com/kata/59cb550486a6fd10b50000ad/train/sql
+--Time: 12164ms Passed: 2 Failed: 0
+
+--PostgreSQL 9.6
+--¹®Á¦ÆÄ¾ÇÀÌ ¾î·Á¿üÀ½. 
+SELECT number
+  FROM numbers 
+ ORDER BY RANDOM()
