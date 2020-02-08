299d6e1e0974bb214b6af4d7b8a85bdda98dff4e
netty1010 <56634703+netty1010@users.noreply.github.com>
2020-02-08 13:42:23 +0900

    [easy] 50. Pow(x, n)
    문제 난이도는 easy, 실제 레벨은 medium

---
+++ leetcode/easy/50_pow/dosun.scala
@@ -0,0 +1,9 @@
+/**
+  * https://leetcode.com/problems/powx-n/submissions/
+  *
+  * Runtime: 20 ms, faster than 96.82% of Python3 online submissions for Pow(x, n).
+  * Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Pow(x, n).
+  */
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return math.pow(x, n)
