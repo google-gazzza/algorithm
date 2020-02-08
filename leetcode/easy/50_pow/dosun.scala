2fc960d14fdc48677942407835e3f3fabc02007f
netty1010 <56634703+netty1010@users.noreply.github.com>
2020-02-08 13:46:34 +0900

    이것아 이건 스칼라다...
    .scala 로 올려놓고 python 코드를 commit 했네요..

--- leetcode/easy/50_pow/dosun.scala
+++ leetcode/easy/50_pow/dosun.scala
@@ -4,6 +4,8 @@
   * Runtime: 20 ms, faster than 96.82% of Python3 online submissions for Pow(x, n).
   * Memory Usage: 12.9 MB, less than 100.00% of Python3 online submissions for Pow(x, n).
   */
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        return math.pow(x, n)
+object Solution {
+    def myPow(x: Double, n: Int): Double = {
+        return math.pow(x, n)   
+    }
+}
