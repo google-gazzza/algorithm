5be5914ae6db0cc2c0bb586ac20b46ed87531110
jgsim <sjg4792@gmail.com>
2020-02-12 17:29:21 +0900

    for문 개선

--- leetcode/easy/204_count_primes/runaway.java
+++ leetcode/easy/204_count_primes/runaway.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/count-primes
-// Runtime: 75 ms, faster than 24.98% of Java online submissions for Count Primes.
-// Memory Usage: 36.6 MB, less than 5.66% of Java online submissions for Count Primes.
+// Runtime: 46 ms, faster than 29.50% of Java online submissions for Count Primes.
+// Memory Usage: 36.7 MB, less than 5.66% of Java online submissions for Count Primes.
 
 class Solution {
     public int countPrimes(int n) {
@@ -10,7 +10,7 @@ class Solution {
         BitSet bits = new BitSet(n);
         bits.set(2, n, true);
         
-        for (int m = 2; m < n; m++) {
+        for (int m = 2; m * m < n; m++) {
             if (!bits.get(m))
                 continue;
             int k = 2;
