3800e1d8095c24fa90b9e9ca63e4769066fad323
donghokim-DBA <dongho527@naver.com>
2020-02-18 23:09:32 +0900

    이전 코드 원복

--- codewars/7kyu/Counting_and_Grouping/dongho.sql
+++ codewars/7kyu/Counting_and_Grouping/dongho.sql
@@ -1,8 +1,7 @@
---https://www.codewars.com/kata/5d50e3914861a500121e1958/train/sql
---Time: 1586ms Passed: 7 Failed: 0
-
+--https://www.codewars.com/kata/594633020a561e329a0000a2/train/sql
 --PostgreSQL 9.6
 
-SELECT (CASE WHEN COUNT(1) = 0 THEN 'z'
-             ELSE CHR(((sum(ascii(letter) - ascii('a') + 1) - 1) % 26 + ascii('a'))::int) END) AS letter 
-  FROM letters
+SELECT race, count(*) AS count
+  FROM  demographics
+ GROUP BY race
+ ORDER BY 2 DESC
