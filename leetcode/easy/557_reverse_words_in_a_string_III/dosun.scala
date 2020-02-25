c524a642bee2d7c415919582a6ed8fc872fdd865
netty1010 <56634703+netty1010@users.noreply.github.com>
2020-02-25 20:04:37 +0900

    코드 리팩토링

--- leetcode/easy/557_reverse_words_in_a_string_III/dosun.scala
+++ leetcode/easy/557_reverse_words_in_a_string_III/dosun.scala
@@ -1,15 +1,11 @@
 /**
   * https://leetcode.com/problems/reverse-words-in-a-string-iii/
   *
-  * Runtime: 4140 ms, faster than 85.71% of Scala online submissions for Reverse Words in a String III.
-  * Memory Usage: 55.4 MB, less than 100.00% of Scala online submissions for Reverse Words in a String III.
+  * Runtime: 4008 ms, faster than 92.86% of Scala online submissions for Reverse Words in a String III.
+  * Memory Usage: 55.1 MB, less than 100.00% of Scala online submissions for Reverse Words in a String III.
   */
 object Solution {
     def reverseWords(s: String): String = {
-        val words  = s.split(" ")
-        var result = List[String]()
-        words.foreach(word => {result = word.reverse :: result})
-
-        result.reverse.mkString(" ")
+        s.split(" ").map(_.reverse).mkString(" ")
     }
 }
