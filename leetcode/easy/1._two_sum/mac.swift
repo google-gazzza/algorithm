8bb079243dde238b7375e38a54b18a89e8a7d11c
bestSwift <best2753@gmail.com>
2020-02-25 23:08:17 +0900

    Update mac.swift

--- leetcode/easy/1._two_sum/mac.swift
+++ leetcode/easy/1._two_sum/mac.swift
@@ -1,6 +1,6 @@
-https://leetcode.com/problems/two-sum/
-Runtime: 32 ms, faster than 95.20% of Swift online submissions for Two Sum.
-Memory Usage: 20.9 MB, less than 5.88% of Swift online submissions for Two Sum.
+// https://leetcode.com/problems/two-sum/
+// Runtime: 32 ms, faster than 95.20% of Swift online submissions for Two Sum.
+// Memory Usage: 20.9 MB, less than 5.88% of Swift online submissions for Two Sum.
 
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
