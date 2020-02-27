e833f70b45fa9048382e7f528cfcd9e0b4ad100a
netty1010 <56634703+netty1010@users.noreply.github.com>
2020-02-27 22:32:32 +0900

    코드 리팩토링 : 가독성 up, 속도 down

--- leetcode/easy/929_unique_email_addresses/dosun.scala
+++ leetcode/easy/929_unique_email_addresses/dosun.scala
@@ -1,32 +1,19 @@
 /**
   * https://leetcode.com/problems/unique-email-addresses/
   * 
-  * Runtime: 596 ms, faster than 60.00% of Scala online submissions for Unique Email Addresses.
-  * Memory Usage: 52.9 MB, less than 100.00% of Scala online submissions for Unique Email Addresses.
+  * Runtime: 624 ms, faster than 40.00% of Scala online submissions for Unique Email Addresses.
+  * Memory Usage: 53.4 MB, less than 100.00% of Scala online submissions for Unique Email Addresses.
   */
 object Solution {
     def numUniqueEmails(emails: Array[String]): Int = {
-        val at   = "@"
-        val plus = "+"
-        val dot  = "."
-
-        val withoutDots = emails.map(e => {
-            val atIndex = e.indexOf(at)
-            val name    = e.substring(0, atIndex).replace(dot, "")
-            val domain  = e.substring(atIndex)
-            name + domain
-        })
-
-        val uniques = withoutDots.map { e => e match {
-            case x if x.contains(plus) => {
-              val ignoredName = e.substring(e.indexOf(plus), e.indexOf(at))
-              e.replace(ignoredName, "")
-            }
-            case _ => e
-        }}.distinct
-
-        var numUnique = 0
-        uniques.foreach(_ => numUnique += 1)
-        numUnique
+        val filteredUniqueEmails = emails.map(e => {
+            val separated = e.split("@")
+            val name      = separated(0).replaceAll("(\\.)|(\\+.*)", "")
+            val domain    = separated(1)
+            name + "@" + domain
+        }).distinct
+        
+        filteredUniqueEmails.size
     }
 }
+
