f07fe7f3b341427a4dd19b0784e791b5e2e65e5e
netty1010 <netty0kaist@gmail.com>
2020-03-16 21:56:24 +0900

    [easy] 461. Hamming Distance

---
+++ leetcode/easy/461_hamming_distance/dosun.py
@@ -0,0 +1,18 @@
+"""
+https://leetcode.com/problems/hamming-distance/
+
+Runtime: 20 ms, faster than 96.47% of Python3 online submissions for Hamming Distance.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Hamming Distance.
+"""
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor = x ^ y
+        count = 0
+        for i in range(32):
+            count += (xor >> i) & 1
+        return count
+
+
+
+
+
