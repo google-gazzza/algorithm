cc2b99d1bcf06173ad5932a908f41e378da8f5e0
byungkyu-ju <noel1227@gmail.com>
2020-03-29 23:38:03 +0900

    [Medium] Solved 199. Binary Tree Right Side View

---
+++ leetcode/medium/199_binary_tree_right_side_view/yalla.java
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/binary-tree-right-side-view/
+//Runtime: 1 ms, faster than 75.35% of Java online submissions for Binary Tree Right Side View.
+//Memory Usage: 38.1 MB, less than 5.88% of Java online submissions for Binary Tree Right Side View.
+//
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        if (root == null) return ret;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                if (i == size - 1) {
+                    ret.add(current.val);
+                }
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+        }
+        return ret;
+    }
+}
