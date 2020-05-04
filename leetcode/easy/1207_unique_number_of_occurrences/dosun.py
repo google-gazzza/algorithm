120121cba038884d4189877aa729cf4c358f0d8f
netty1010 <56634703+netty1010@users.noreply.github.com>
2020-05-04 22:32:30 +0900

    변수명 수정

--- leetcode/easy/1207_unique_number_of_occurrences/dosun.py
+++ leetcode/easy/1207_unique_number_of_occurrences/dosun.py
@@ -7,6 +7,6 @@ Memory Usage: 13.9 MB, less than 100.00% of Python3 online submissions for Uniqu
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         arr_counter_values = Counter(arr).values()
-        unique_arr_count_values = set(arr_counter_values)
+        unique_arr_counter_values = set(arr_counter_values)
      
-        return len(arr_counter_values) == len(unique_arr_count_values)
+        return len(arr_counter_values) == len(unique_arr_counter_values)
