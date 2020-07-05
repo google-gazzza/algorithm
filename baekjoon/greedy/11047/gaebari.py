cec35c74b93b65434a3c9c5a7782d2f880fc0ba3
jmp7786 <jmp7786@gmail.com>
2020-07-06 01:41:20 +0900

    [boj] 1931 회의실 배정

---
+++ baekjoon/greedy/11047/gaebari.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/11047
+# 동전 0
+input_data = input().split(' ')
+coinNumber = int(input_data[0])
+targetMoney = int(input_data[1])
+coins = [int(input()) for i in range(coinNumber)]
+
+result = 0
+idx = 0
+count = 0
+quotient = 0
+while targetMoney > 0:
+    if targetMoney <= 0:
+        break
+    quotient = targetMoney // coins[-(idx + 1)]
+    targetMoney %= coins[-(idx + 1)]
+    idx += 1
+    count += quotient
+
+print(count)
+
+
+
