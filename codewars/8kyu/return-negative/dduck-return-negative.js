53faacea51f50a42eb9b18db991392e2f03dd6a0
codename6027 <codename6027@gmail.com>
2020-07-30 00:09:39 +0900

    feat(codewars/8kyu/return-negative.js)

---
+++ codewars/8kyu/return-negative/dduck-return-negative.js
@@ -0,0 +1,10 @@
+// return-negative
+// Return Negative
+// difficulty: 8kyu
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/
+// approach: https://blog.devserum.com/data-structures-and-algorithms/coding-challenges/codewars/8kyu/return-negative
+
+const makeNegative = (num) => Math.abs(num) * -1;
+
+
+console.log(makeNegative(42), -42);
