70f667ef955ccc0525b17a8bb9e15832587cdef4
codename6027 <codename6027@gmail.com>
2020-07-31 02:49:32 +0900

    feat(codewars/8kyu/convert-a-number-to-a-string.js)

---
+++ codewars/8kyu/convert-a-number-to-a-string/convert-a-number-to-a-string.js
@@ -0,0 +1,12 @@
+// convert-a-number-to-a-string
+// Convert a Number to a String!
+// difficulty: 8kyu
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/
+// approach: https://blog.devserum.com/data-structures-and-algorithms/coding-challenges/codewars/8kyu/convert-a-number-to-a-string
+
+const numberToString = (num) => num.toString();
+
+
+
+console.log(numberToString(67), '67');
+//?
