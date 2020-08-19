aae8d55106dd0cdd31e25900034dd624a9d386bf
codename6027 <codename6027@gmail.com>
2020-08-20 03:08:00 +0900

    feat(codewars/7kyu/most-digits.js)

---
+++ codewars/7kyu/most-digits/dduck-most-digits.js
@@ -0,0 +1,13 @@
+// most-digits
+// Most digits
+// difficulty: 7kyu
+// https://www.codewars.com/kata/58daa7617332e59593000006/
+// approach: https://blog.devserum.com/data-structures-and-algorithms/coding-challenges/codewars/8kyu/watermelon
+
+const findLongest = (array) => Math.max(...array);
+
+
+
+console.log(findLongest([1, 10, 100]), 100);
+console.log(findLongest([9000, 8, 800]), 9000);
+console.log(findLongest([8, 900, 500]), 900);
