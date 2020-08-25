05bba7e8cd61230b8ff8d0af86598841237cbaef
codename6027 <codename6027@gmail.com>
2020-08-26 02:10:25 +0900

    feat(codewars/7kyu/sum-of-angles.js)

---
+++ codewars/7kyu/sum-of-angles/dduck-sum-of-angles.js
@@ -0,0 +1,11 @@
+// sum-of-angles
+// Sum of angles
+// difficulty: 7kyu
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765/
+
+const angle = (n) => (n - 2) * 180;
+
+
+
+console.log(angle(3), 180);
+console.log(angle(4), 360);
