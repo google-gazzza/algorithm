00f5129f0aeac903efb86c404c423b8c68e335e4
codename6027 <codename6027@gmail.com>
2020-08-29 04:24:33 +0900

    feat(codewars/8kyu/convert-to-binary.js)

---
+++ codewars/8kyu/convert-to-binary/dduck-convert-to-binary.js
@@ -0,0 +1,11 @@
+// convert-to-binary
+// Convert to Binary
+// https://www.codewars.com/kata/59fca81a5712f9fa4700159a
+
+const toBinary = (n) => Number(n.toString(2));
+
+
+console.log(toBinary(1), 1);
+console.log(toBinary(2), 10);
+console.log(toBinary(3), 11);
+console.log(toBinary(5), 101);
