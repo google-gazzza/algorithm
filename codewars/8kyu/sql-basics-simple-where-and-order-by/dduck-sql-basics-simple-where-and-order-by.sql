f772284ecc759d0691b27a6312e45659b2c3f656
codename6027 <codename6027@gmail.com>
2020-09-08 01:34:25 +0900

    feat(codewars/8kyu/sql-basics-simple-where-and-order-by.sql)

---
+++ codewars/8kyu/sql-basics-simple-where-and-order-by/dduck-sql-basics-simple-where-and-order-by.sql
@@ -0,0 +1,8 @@
+-- sql-basics-simple-where-and-order-by
+-- SQL Basics: Simple WHERE and ORDER BY
+-- https://www.codewars.com/kata/5809508cc47d327c12000084
+
+SELECT id, name, age
+FROM people
+WHERE age > 50
+ORDER BY age DESC
