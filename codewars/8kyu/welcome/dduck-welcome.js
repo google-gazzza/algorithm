4403a273efd86b22bed60006db3f6d43e162c522
codename6027 <codename6027@gmail.com>
2020-09-12 03:44:33 +0900

    feat(codewars/8kyu/welcome.js)

---
+++ codewars/8kyu/welcome/dduck-welcome.js
@@ -0,0 +1,32 @@
+// welcome
+// Welcome!
+// difficulty: 8kyu
+// https://www.codewars.com/kata/577ff15ad648a14b780000e7
+
+const database = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso',
+};
+
+const greet = (language) => database[language] || database.english;
+
+
+
+console.log(greet('english'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
+console.log(greet('dutch'), 'Welkom', "Your function should have returned 'Welkom'. Try again.");
+console.log(greet('IP_ADDRESS_INVALID'), 'Welcome', "Your function should have returned 'Welcome'. Try again.");
