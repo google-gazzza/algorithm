9e3e8c3eb65f431fe4165c122e32f543620ee0ea
codename6027 <codename6027@gmail.com>
2021-05-16 02:07:32 +0900

    refactor(leetcode/easy/two-sum/two-sum.js)

--- leetcode/easy/1._two_sum/dduck-two-sum.js
+++ leetcode/easy/1._two_sum/dduck-two-sum.js
@@ -23,30 +23,49 @@ URL: https://leetcode.com/problems/two-sum/
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i += 1) {
-    for (let j = i + 1; j < nums.length; j += 1) {
-      const sum = (nums[i] + nums[j]);
-      if (sum === target) {
-        return [i, j];
+
+
+// ------------------------------
+// hashing
+// ------------------------------
+function twoSum(nums, target) {
+  const set = new Set(nums);
+  let result;
+  
+  nums.some((e) => {
+    if (set.has(target - e)) {
+      const left = nums.indexOf(e);
+      const right = nums.lastIndexOf(target - e);
+      
+      if (left !== right) {
+        result = [left, right];
+        return true;
       }
     }
-  }
-};
+  });
+  
+  return result;
+}
 
-// test
-console.log(twoSum([2, 7, 11, 15], 9));
-// ?
-// [0, 1]
-console.log(twoSum([3, 2, 4], 6));
-// ?
-// [1, 2]
-console.log(twoSum([3, 3], 6));
-// ?
-// [0, 1]
-console.log(twoSum([2, 5, 5, 11], 10));
-//?
-// [1, 2]
-console.log(twoSum([0, 4, 3, 0], 0));
-//?
-// [0, 3]
+// ------------------------------
+// brute force
+// ------------------------------
+// const twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i += 1) {
+//     for (let j = i + 1; j < nums.length; j += 1) {
+//       const sum = (nums[i] + nums[j]);
+//       if (sum === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
+
+
+test('test', () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
+  expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2]);
+  expect(twoSum([3, 3], 6)).toStrictEqual([0, 1]);
+  expect(twoSum([2, 5, 5, 11], 10)).toStrictEqual([1, 2]);
+  expect(twoSum([0, 4, 3, 0], 0)).toStrictEqual([0, 3]);
+});
