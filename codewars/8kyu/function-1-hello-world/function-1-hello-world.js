4236292e0e28e39a36195cb9d4b117c3088bfa25
codename6027 <codename6027@gmail.com>
2021-05-19 03:49:20 +0900

    feat(codewars/8kyu/function-1-hello-world.js)

---
+++ codewars/8kyu/function-1-hello-world/function-1-hello-world.js
@@ -0,0 +1,21 @@
+/*
+function-1-hello-world
+
+# codewars/8kyu/Function 1 - hello world
+Difficulty: 8kyu
+URL: https://www.codewars.com/kata/523b4ff7adca849afe000035/
+Tags:
+
+## Approach
+
+### en
+
+### kr
+
+## Solution
+### JavaScript
+*/
+
+function greet() {
+  return 'hello world!';
+}
