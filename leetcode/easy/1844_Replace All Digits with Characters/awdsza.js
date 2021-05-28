1a0de022faae154a2cb73c904e11d7537ebb8e4e
awdsza <37355034+awdsza@users.noreply.github.com>
2021-05-28 17:54:45 +0900

    Update awdsza.js

--- leetcode/easy/1844_Replace All Digits with Characters/awdsza.js
+++ leetcode/easy/1844_Replace All Digits with Characters/awdsza.js
@@ -11,20 +11,19 @@ For every odd index i, you want to replace the digit s[i] with shift(s[i-1], s[i
 Return s after replacing all digits. It is guaranteed that shift(s[i-1], s[i]) will never exceed 'z'.
  */
 /*
-ex) s
+Runtime: 72 ms, faster than 94.30% of JavaScript online submissions for Replace All Digits with Characters.
+Memory Usage: 38.7 MB, less than 52.01% of JavaScript online submissions for Replace All Digits with Characters.
 */
-/*본인 작성*/
+
 var replaceDigits = function(s) {
     let result = [];
-    for(let i = 0; i<s.length;i++){
-        let char = s[i];
+    s.split('').forEach((char,i,s)=>{
         if(i%2 !== 0){//짝수일때
             char=shift(s[i-1],Number(char));
         }
-        result.push(char);    
-    }return result.join('');
-    
-    
+        result.push(char);
+    });
+    return result.join('');
 };
 /*character에서 shiftNumber만큼 숫자를 이동시키는 함수*/
 let shift = function(character,shiftNumber){
