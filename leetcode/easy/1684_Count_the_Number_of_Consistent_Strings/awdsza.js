b0a31ae79274dc6ab88ff564e5bfe0a7ddfebf81
awdsza <37355034+awdsza@users.noreply.github.com>
2021-06-11 13:03:34 +0900

    Create awdsza.js

---
+++ leetcode/easy/1684_Count_the_Number_of_Consistent_Strings/awdsza.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+/*
+Runtime: 148 ms, faster than 23.90% of JavaScript online submissions for Count the Number of Consistent Strings.
+Memory Usage: 47.2 MB, less than 98.41% of JavaScript online submissions for Count the Number of Consistent Strings.
+*/
+var countConsistentStrings = function(allowed, words) {
+  let result = 0;
+    words.forEach((word)=>{
+        let isConsistent=true;
+        for(let i=0;i<word.length;i++){
+            let char = word.charAt(i);
+            if(allowed.indexOf(char) === -1){
+               isConsistent=false;
+                break;
+           }
+        }
+        result += (isConsistent ? 1 : 0);
+    })
+    return result;
+};
