3502d079f9fa40982da1b7d35edb8b2110d5ab24
codename6027 <codename6027@gmail.com>
2021-08-25 01:28:59 +0900

    feat(leetcode/easy/1929-concatenation-of-array.py)

---
+++ leetcode/easy/1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,10 @@
+"""
+1929-concatenation-of-array
+leetcode/easy/1929. Concatenation of Array
+Difficulty:  easy
+URL: https://leetcode.com/problems/concatenation-of-array/
+"""
+
+class Solution:
+    def getConcatenation(self, nums):
+        return nums * 2
