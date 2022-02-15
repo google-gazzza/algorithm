315652965afc7ac434a9da6b08b3c3dd07b05cc8
koronya <koronya@hanmail.net>
2022-02-16 00:25:56 +0900

    [TS][Medium] 1663. Smallest String With A Given Numeric Value

---
+++ leetcode/medium/1663_smallest_string_with_a_given_numeric_value/koronya.ts
@@ -0,0 +1,39 @@
+// [Medium] 1663. Smallest String With A Given Numeric Value
+// 1663_smallest_string_with_a_given_numeric_value
+
+// https://leetcode.com/problems/smallest-string-with-a-given-numeric-value
+// Runtime: 124 ms, faster than 100.00% of TypeScript online submissions for Smallest String With A Given Numeric Value.
+// Memory Usage: 49.7 MB, less than 50.00% of TypeScript online submissions for Smallest String With A Given Numeric Value.
+
+const CHAR_CODE_OF_A = 'a'.charCodeAt(0)
+const LENGTH_OF_ALPHABET = 26
+
+const getCharFromNumber = (num: number): string => {
+  return String.fromCharCode(num - 1 + CHAR_CODE_OF_A)
+}
+
+const getString = (num: number, len: number): string => {
+  let str = ''
+  const limit = len - 1
+  for (let i = 0; i < limit; i += 1 || 0) {
+    str += 'a'
+  }
+  return str + getCharFromNumber(num - len + 1)
+}
+
+function getSmallestString(n: number, k: number): string {
+  let target = k
+  let 몫 = Math.floor(target / LENGTH_OF_ALPHABET)
+  let 나머지 = target % LENGTH_OF_ALPHABET
+  if (나머지 === 0 && 몫 === n) {
+    return 'z'.repeat(몫)
+  }
+  while (true) {
+    if (나머지 + 몫 >= n) {
+      return getString(나머지, n - 몫) + 'z'.repeat(몫)
+    } else {
+      몫 -= 1
+      나머지 += 26
+    }
+  }
+}
