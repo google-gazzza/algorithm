fcc2ae63750e9a3cc107af531c53130ecd32f0be
koronya <koronya@hanmail.net>
2022-04-21 23:26:19 +0900

    [TS][Easy] 2235. Add Two Integers

---
+++ leetcode/easy/2235_add_two_integers/koronya.ts
@@ -0,0 +1,10 @@
+// [Easy] 2235. Add Two Integers
+// 2235_add_two_integers
+
+// https://leetcode.com/problems/add-two-integers
+// Runtime: 81 ms, faster than 29.85% of TypeScript online submissions for Add Two Integers.
+// Memory Usage: 42.8 MB, less than 86.57% of TypeScript online submissions for Add Two Integers.
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2
+}
