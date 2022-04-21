fd1c4345e5b6826eafbae5a1ad3afc1c82a40866
koronya <koronya@hanmail.net>
2022-04-21 23:25:58 +0900

    [JS][Easy] 2235. Add Two Integers

---
+++ leetcode/easy/2235_add_two_integers/koronya.js
@@ -0,0 +1,15 @@
+// [Easy] 2235. Add Two Integers
+// 2235_add_two_integers
+
+// https://leetcode.com/problems/add-two-integers
+// Runtime: 60 ms, faster than 86.57% of JavaScript online submissions for Add Two Integers.
+// Memory Usage: 42.5 MB, less than 19.58% of JavaScript online submissions for Add Two Integers.
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+const sum = (num1, num2) => num1 + num2
+
+sum(12, 5)
