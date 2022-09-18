4e5baed04e180901c274302e956040e184c99184
koronya <koronya@hanmail.net>
2022-09-19 04:26:36 +0900

    [JS][8kyu] Keep Hydrated!

---
+++ codewars/8kyu/keep-hydrated/koronya.js
@@ -0,0 +1,13 @@
+// [JS][8kyu] Keep Hydrated!
+// keep-hydrated
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
+
+const litres = (time) => Math.floor(time / 2)
+
+litres(2) === 1
+litres(1.4) === 0
+litres(12.3) === 6
+litres(0.82) === 0
+litres(11.8) === 5
+litres(1787) === 893
+litres(0) === 0
