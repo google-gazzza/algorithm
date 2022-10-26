d4ca994ad94e5ab8cfcd0036152796fefea9b9db
koronya <koronya@hanmail.net>
2022-10-27 02:38:32 +0900

    [JS][8kyu] Switch it Up!

---
+++ codewars/8kyu/switch-it-up/koronya.js
@@ -0,0 +1,33 @@
+// [JS][8kyu] Switch it Up!
+// switch-it-up
+// https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
+
+const switchItUp = (number) => {
+  switch (number) {
+    case 0:
+      return 'Zero'
+    case 1:
+      return 'One'
+    case 2:
+      return 'Two'
+    case 3:
+      return 'Three'
+    case 4:
+      return 'Four'
+    case 5:
+      return 'Five'
+    case 6:
+      return 'Six'
+    case 7:
+      return 'Seven'
+    case 8:
+      return 'Eight'
+    case 9:
+      return 'Nine'
+  }
+  return 3
+}
+
+switchItUp(1)
+switchItUp(3)
+switchItUp(5)
