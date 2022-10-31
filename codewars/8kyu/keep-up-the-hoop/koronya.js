164723b81d067a07f5fb8d1506362816d4893d30
koronya <koronya@hanmail.net>
2022-11-01 01:42:44 +0900

    [JS][8kyu] Keep up the hoop

---
+++ codewars/8kyu/keep-up-the-hoop/koronya.js
@@ -0,0 +1,8 @@
+// [JS][8kyu] Keep up the hoop
+// keep-up-the-hoop
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
+
+const hoopCount = (n) => (n >= 10 ? 'Great, now move on to tricks' : 'Keep at it until you get it')
+
+hoopCount(3)
+hoopCount(11)
