706b23be18d016d39ebe0bd3c6c7d347a2c083d5
koronya <koronya@hanmail.net>
2023-03-01 20:21:04 +0900

    [JS][7kyu] Breaking chocolate problem

---
+++ codewars/7kyu/breaking-chocolate-problem/koronya.js
@@ -0,0 +1,9 @@
+// [JS][7kyu] Breaking chocolate problem
+// breaking-chocolate-problem
+// https://www.codewars.com/kata/534ea96ebb17181947000ada/train/javascript
+
+const breakChocolate = (n, m) => Math.max(n * m - 1, 0)
+
+breakChocolate(5, 5)
+breakChocolate(1, 1)
+breakChocolate(0, 3)
