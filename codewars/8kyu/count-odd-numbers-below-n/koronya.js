2dec30c286ffbc8e95dd228af7b561ded7fbfc20
koronya <koronya@hanmail.net>
2023-04-20 22:50:37 +0900

    [JS][8kyu] Count Odd Numbers below n

---
+++ codewars/8kyu/count-odd-numbers-below-n/koronya.js
@@ -0,0 +1,8 @@
+// [JS][8kyu] Count Odd Numbers below n
+// count-odd-numbers-below-n
+// https://www.codewars.com/kata/59342039eb450e39970000a6/train/javascript
+
+const oddCount = (n) => Math.floor(n / 2)
+
+oddCount(15) === 7
+oddCount(15023) === 7511
