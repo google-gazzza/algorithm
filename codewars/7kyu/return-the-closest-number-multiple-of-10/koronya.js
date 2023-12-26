eac070350ae6c18bd49aab1ccdbef19e5fc7d949
koronya <koronya@hanmail.net>
2023-12-26 16:44:07 +0900

    [JS][7kyu] Return the closest number multiple of 10

---
+++ codewars/7kyu/return-the-closest-number-multiple-of-10/koronya.js
@@ -0,0 +1,9 @@
+// [JS][7kyu] Return the closest number multiple of 10
+// return-the-closest-number-multiple-of-10
+// https://www.codewars.com/kata/58249d08b81f70a2fc0001a4/train/javascript
+
+const closestMultiple10 = (num) => Math.round(num / 10) * 10
+
+closestMultiple10(22)
+closestMultiple10(25)
+closestMultiple10(37)
