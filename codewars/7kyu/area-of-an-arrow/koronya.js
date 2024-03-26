d4ea3566a883346826bd250a2fbbc7d7327c5f38
koronya <koronya@hanmail.net>
2024-03-27 01:37:25 +0900

    [JS][7kyu] Area of an arrow

---
+++ codewars/7kyu/area-of-an-arrow/koronya.js
@@ -0,0 +1,10 @@
+// [JS][7kyu] Area of an arrow
+// area-of-an-arrow
+// https://www.codewars.com/kata/589478160c0f8a40870000bc/train/javascript
+
+const arrowArea = (a, b) => (a * b) / 4
+// return a * (b / 2) / 2
+
+arrowArea(4, 2) === 2
+arrowArea(7, 6) === 10.5
+arrowArea(25, 25) === 156.25
