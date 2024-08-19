626a8d8b4b3113c9d059e7aae03bcb469ee38020
koronya <koronya@hanmail.net>
2024-08-20 04:10:16 +0900

    [JS][7kyu] Thinkful - String Drills: Quotable

---
+++ codewars/7kyu/thinkful-string-drills-quotable/koronya.js
@@ -0,0 +1,11 @@
+// [JS][7kyu] Thinkful - String Drills: Quotable
+// thinkful-string-drills-quotable
+// https://www.codewars.com/kata/5859c82bd41fc6207900007a/train/javascript
+
+const quotable = (name, quote) => `${name} said: "${quote}"`
+
+quotable('Grae', 'Practice makes perfect') === 'Grae said: "Practice makes perfect"'
+quotable('Dan', 'Get back to work, Grae') === 'Dan said: "Get back to work, Grae"'
+quotable('Alex', 'Ruby is great fun') === 'Alex said: "Ruby is great fun"'
+quotable('Bethany', 'Yes, way more fun than R') === 'Bethany said: "Yes, way more fun than R"'
+quotable('Darrell', 'What the heck is this thing?') === 'Darrell said: "What the heck is this thing?"'
