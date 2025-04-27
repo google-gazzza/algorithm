c475545d58675ba0355b7ae0a19fe7507295ddfe
koronya <koronya@hanmail.net>
2025-04-27 18:39:02 +0900

    [Medium] 1143. Longest Common Subsequence

--- leetcode/medium/1143_longest_common_subsequence/koronya.ts
+++ leetcode/medium/1143_longest_common_subsequence/koronya.ts
@@ -2,15 +2,10 @@
 // 1143_longest_common_subsequence
 
 // https://leetcode.com/problems/longest-common-subsequence/description/
-// Runtime: 33 ms, faster than 88.25%
-// Memory Usage: 82.13 MB, less than 67.28%
+// Runtime: 49 ms, faster than 75.07%
+// Memory Usage: 82.83 MB, less than 60.70%
 
-/**
- * @param {string} text1
- * @param {string} text2
- * @return {number}
- */
-const longestCommonSubsequence = (text1, text2) => {
+function longestCommonSubsequence(text1: string, text2: string): number {
   const text1Length = text1.length
   const text2Length = text2.length
   const dp = Array.from({ length: text1Length + 1 }, () => Array(text2Length + 1).fill(0))
