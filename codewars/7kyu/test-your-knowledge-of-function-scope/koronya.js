dd744a6443182908fa287200107cd3b7a7068a69
koronya <koronya@hanmail.net>
2025-11-15 04:23:34 +0900

    [JS][7kyu] Test Your Knowledge Of Function Scope

---
+++ codewars/7kyu/test-your-knowledge-of-function-scope/koronya.js
@@ -0,0 +1,8 @@
+// [JS][7kyu] Test Your Knowledge Of Function Scope
+// test-your-knowledge-of-function-scope
+// https://www.codewars.com/kata/56d344c7fd3a52566700124b/train/javascript
+
+const add = (a) => (b) => a + b
+
+add(3)(4)
+add(12)(20)
