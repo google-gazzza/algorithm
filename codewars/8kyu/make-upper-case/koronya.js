b9185f2000802db434397965a165a889e9194adc
koronya <koronya@hanmail.net>
2025-12-17 16:32:31 +0900

    [JS][8kyu] MakeUpperCase

---
+++ codewars/8kyu/make-upper-case/koronya.js
@@ -0,0 +1,11 @@
+// [JS][8kyu] MakeUpperCase
+// make-upper-case
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+
+const makeUpperCase = (str) => str.toUpperCase()
+// Code here
+
+makeUpperCase('') === ''
+makeUpperCase('hello') === 'HELLO'
+makeUpperCase('Hello') === 'HELLO'
+makeUpperCase('HELLO') === 'HELLO'
