99687302dd5d87d3e46da9b7dc96dc2b5b3fa33e
koronya <koronya@hanmail.net>
2026-02-06 04:29:20 +0900

    [JS][7kyu] Make them bark!

---
+++ codewars/7kyu/make-them-bark/koronya.js
@@ -0,0 +1,7 @@
+// [JS][7kyu] Make them bark!
+// make-them-bark
+// https://www.codewars.com/kata/5535572c1de94ba2db0000f6/train/javascript
+
+Dog.prototype.bark = function () {
+  return 'Woof!'
+}
