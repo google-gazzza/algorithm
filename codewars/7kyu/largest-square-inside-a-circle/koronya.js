38445600bfe2dd356a4091d7d8717e2665b078b0
koronya <koronya@hanmail.net>
2026-04-23 23:39:40 +0900

    [JS][7kyu] Largest Square Inside A Circle

---
+++ codewars/7kyu/largest-square-inside-a-circle/koronya.js
@@ -0,0 +1,9 @@
+// [JS][7kyu] Largest Square Inside A Circle
+// largest-square-inside-a-circle
+// https://www.codewars.com/kata/5887a6fe0cfe64850800161c/train/javascript
+
+const areaLargestSquare = (r) => 2 * r ** 2
+
+areaLargestSquare(5) === 50
+areaLargestSquare(7) === 98
+areaLargestSquare(15) === 450
